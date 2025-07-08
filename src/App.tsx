80d497191afc9d0a9315edfec8c3b9426a07f8bb
Parth Mane <parth.mane@sprinklr.com>
2025-07-08 15:09:53 +0530

    chore: Comment out socket stuff

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,38 @@ import { useCallback, useEffect, useState } from 'react';
 import { TemplateRenderer } from '@/components/molecules/TemplateRenderer';
 import { Layout } from '@/Layout';
 
-const socket = new WebSocket(`ws://localhost:3000/socket`);
-
-function Counter() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const onOpen = () => {
-      console.log('Connected');
-    };
-    socket.addEventListener('open', onOpen);
-
-    const onMessage = (message: MessageEvent) => {
-      setCount(x => x + 1);
-    };
-    socket.addEventListener('message', onMessage);
-
-    return () => {
-      socket.removeEventListener('open', onOpen);
-      socket.removeEventListener('message', onMessage);
-    };
-  });
-
-  const send = useCallback(() => {
-    socket.send('Test');
-  }, []);
-
-  return (
-    <div>
-      {count} <button onClick={send}>Clickity click</button>{' '}
-    </div>
-  );
-}
+// const socket = new WebSocket(`ws://localhost:${Bun.env.BUN_PORT || 3000}/socket`);
+//
+// function Counter() {
+//   const [count, setCount] = useState(0);
+//
+//   useEffect(() => {
+//     const onOpen = () => {
+//       console.log('Connected');
+//     };
+//     socket.addEventListener('open', onOpen);
+//
+//     const onMessage = (message: MessageEvent) => {
+//       setCount(x => x + 1);
+//     };
+//     socket.addEventListener('message', onMessage);
+//
+//     return () => {
+//       socket.removeEventListener('open', onOpen);
+//       socket.removeEventListener('message', onMessage);
+//     };
+//   });
+//
+//   const send = useCallback(() => {
+//     socket.send('Test');
+//   }, []);
+//
+//   return (
+//     <div>
+//       {count} <button onClick={send}>Clickity click</button>{' '}
+//     </div>
+//   );
+// }
 
 export function App() {
   return (
