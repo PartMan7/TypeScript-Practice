d3a48a987318695d4b29f022767c30520745258f
shruti.shreyasi <shruti.shreyasi@sprinklr.com>
2025-07-08 16:49:29 +0530

    chore: array pop

---
+++ templates/pop.template.ts
@@ -0,0 +1,10 @@
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type Last<T extends unknown[]> = /* LONG_INPUT */;
+
+type Cases = [
+  Expect<Equal<Last<[]>, never>>,
+  Expect<Equal<Last<[2]>, 2>>,
+  Expect<Equal<Last<[3, 2, 1]>, 1>>,
+  Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
+];
