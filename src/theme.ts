3c3ba7ba919d648e9c796bf19e35629dcf6ceaf2
Parth Mane <parth.mane@sprinklr.com>
2025-07-09 16:09:31 +0530

    chore: Fix FOUC

--- src/theme.ts
+++
@@ -1,4 +0,0 @@
-const pref = localStorage.getItem('theme');
-if (pref === 'light') document.body.classList.add('light');
-else document.body.classList.add('dark');
-setTimeout(() => document.body.classList.add('duration-400'), 0);
