916516ec6cfc38e0430683f34d92c8bf787d7f8c
Parth Mane <parth.mane@sprinklr.com>
2025-07-11 14:46:09 +0530

    feat: Add intro and metadata

--- src/pages/Exercises.tsx
+++ src/pages/Exercises.tsx
@@ -12,10 +12,13 @@ export function Exercises() {
         'Loading exercises...'
       ) : (
         <>
-          <div className="m-4 flex justify-around w-full max-w-196">
+          <div className="m-4 flex flex-wrap justify-around w-full max-w-196 gap-6">
             {exercises.map(exercise => (
               <a href={`/exercise/${exercise}`} key={exercise}>
-                <Button value={exercise} className="cursor-pointer h-16 w-42 flex items-center justify-center text-lg uppercase">
+                <Button
+                  value={exercise}
+                  className="cursor-pointer h-16 w-42 flex items-center justify-center text-lg uppercase"
+                >
                   {exercise}
                 </Button>
               </a>
---
+++ templates/intro.template.ts
@@ -0,0 +1,9 @@
+import type { Equal, Expect } from '@type-challenges/utils';
+
+type HelloWorld = {
+  /* LONG_INPUT */
+};
+
+/* Test Cases */
+
+type Cases = [Expect<Equal<HelloWorld, string>>];
