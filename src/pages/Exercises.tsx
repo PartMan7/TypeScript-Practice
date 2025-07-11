7dc0bf8391503487af88b8508937f5123b87968f
Parth Mane <parth.mane@sprinklr.com>
2025-07-11 15:06:48 +0530

    chore: Add rounded corners to cards

--- src/pages/Exercises.tsx
+++ src/pages/Exercises.tsx
@@ -22,11 +22,11 @@ export function Exercises() {
             {exercises.map(exercise => (
               <a
                 href={`/exercise/${exercise.path.replace(/\.template\.ts$/, '')}`}
-                className="cursor-pointer h-32 w-42 flex flex-col items-center justify-center bg-primary text-primary-foreground shadow-sm hover:bg-primary/90 p-2 relative"
+                className="cursor-pointer h-32 w-42 flex flex-col items-center justify-center bg-primary text-primary-foreground shadow-sm hover:bg-primary/90 p-2 relative rounded-md"
                 key={exercise.path}
               >
                 <span className="absolute top-0 right-2 text-sm">{'☆'.repeat(exercise.difficulty)}</span>
-                <span className="text-lg uppercase">{exercise.name}</span>
+                <span className="text-lg uppercase font-semibold">{exercise.name}</span>
                 {exercise.desc ? (
                   <>
                     <span />
